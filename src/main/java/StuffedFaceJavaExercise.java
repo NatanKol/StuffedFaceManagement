da616e56d9b7d9800fc75df5617579c57454d053
Matthew.McCormick <matthew.mccormick@ten10.com>
2023-06-28 10:55:16 +0100

    Added java file and README.md

---
+++ src/main/java/StuffedFaceJavaExercise.java
@@ -0,0 +1,26 @@
+public class StuffedFaceJavaExercise {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Stuffed Face menu");
+
+        //Summary: Stuffed Face is now looking at taking online orders.
+        //Your task is to be able to create these order items and inform the user of the total cost
+
+        //Exercise is to be completed all within the main method
+        //John orders Burger and Chips
+        //1. Create two menu items:
+        //  a. Burger - 3.49
+        //  b. Chips - 1.99
+        //2. Create order item for "John" with one burger and one chips
+        //3. Print out the order
+        //4. Calculate the total price
+        //5. Calculate and print the 10% service charge and add it to the total
+        //6. Add a new menu item - Milkshake
+        //7. Create a new order
+        //8. Create a user input asking what they would like to order, then print out the cost
+        //9. The user would like to order more than one item, prompt the user if they want to order anything else
+        //10. calculate the running total and summary of the items with service charge when the user has finished
+        //11. Ask the user whether they want to add a service charge with 4 options No | 5% | 10% | 15%
+        //12. Your competitor tries to break your system by inputting invalid values,
+        // see if you can prevent this from occurring by handling any errors!
+    }
+}
